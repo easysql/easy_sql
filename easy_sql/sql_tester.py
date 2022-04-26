4f320a27b0b66287e68ba419c4cb4555523940ab
Shiyun Li <shyli@thoughtworks.com>
2022-04-26 11:21:55 +0800

    clean previous input and output data before creating new input

--- easy_sql/sql_tester.py
+++ easy_sql/sql_tester.py
@@ -557,7 +557,7 @@ class TestCaseRunner:
         sql = case.read_sql_content()
 
         backend = self.backend_creator(case)
-        self.clean_previous_input(case, backend)
+        self.clean_previous_data(case, backend)
         self.create_inputs(case, backend)
 
         sql_processor = self.create_sql_processor(backend, case, sql)
@@ -633,21 +633,22 @@ class TestCaseRunner:
                 print(f'creating temp table: {input.name}')
                 backend.create_temp_table_with_data(input.name, input.values, schema)
 
-    def clean_previous_input(self, case, backend):
-        for input in case.inputs:
-            if '.' in input.name:
-                db = input.name.split(".")[0]
-                if backend.is_bigquery_backend:
-                    try:
-                        backend.exec_native_sql(f"drop schema if exists {db} cascade")
-                    except Exception as e:
-                        # BigQuery will throw an exception when deleting a nonexistent dataset even if using [IF EXISTS]
-                        import re
-                        if re.match(r"[\s\S]*Permission bigquery.datasets.delete denied on dataset[\s\S]*(or it may not exist)[\s\S]*",
+    def clean_previous_data(self, case, backend):
+        databases = set()
+        table_names = set.union(set(input.name for input in case.inputs), set(output.name for output in case.outputs))
+        for table_name in table_names:
+            if '.' in table_name:
+                databases.add(table_name.split(".")[0])
+        if backend.is_bigquery_backend:
+            for db in databases:
+                try:
+                    backend.exec_native_sql(f"drop schema if exists {db} cascade")
+                except Exception as e:
+                    # BigQuery will throw an exception when deleting a nonexistent dataset even if using [IF EXISTS]
+                    import re
+                    if not re.match(r"[\s\S]*Permission bigquery.datasets.delete denied on dataset[\s\S]*(or it may not exist)[\s\S]*",
                                     str(e.args[0])):
-                            return []
-                        else:
-                            raise e
+                        raise e
 
 
 class SqlTester:
@@ -786,4 +787,3 @@ class SqlReader:
 
     def read_as_file_path(self, test_data_file: str):
         return not self.read_as_content(test_data_file)
-
