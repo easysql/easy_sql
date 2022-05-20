d9b6ec7e5320f5baa557f52e8515f88abaf174c1
yangqinke <qinke.yang@thoughtworks.com>
2022-05-20 15:43:57 +0800

    revert func_common

--- easy_sql/sql_processor/funcs_common.py
+++ easy_sql/sql_processor/funcs_common.py
@@ -1,11 +1,9 @@
 import traceback
 from datetime import datetime, timedelta
 from typing import List, Optional, Union
-
 from . import SqlProcessorException, Step
 from .backend import Backend
 from ..logger import logger
-
 __all__ = [
     'ColumnFuncs', 'TableFuncs', 'PartitionFuncs', 'AlertFunc'
 ]
@@ -33,32 +31,6 @@ class ColumnFuncs:
         return ', '.join([f'{pure_table_name}.{col} as `{prefix}{col}`'
                           for col in fields
                           if col not in cols_to_exclude or (col.find('.') != -1 and col.split('.')[-1] not in cols_to_exclude)])
-
-
-    def model_predict_with_tmp_spark(self, model_save_path: str, table_name: str, feature_cols: str, id_col: str, output_ref_cols: str):
-        from pyspark.ml import PipelineModel
-        from pyspark.sql.functions import expr
-        # from pyspark.sql import SparkSession
-        #
-        # spark = SparkSession.builder.getOrCreate()
-
-        output_ref_cols = [col.strip() for col in output_ref_cols.split(',') if col.strip()]
-        model = PipelineModel.load(model_save_path)
-        data = self.backend.exec_native_sql(f'select {feature_cols} from {table_name}')
-        print("select data is")
-        print(data)
-
-        is_int_type = lambda type_name: any([type_name.startswith(t) for t in ['integer', 'long', 'decimal', 'short']])
-        int_cols = [f.name for f in data.schema.fields if is_int_type(f.dataType.typeName())]
-        for col in int_cols:
-            data = data.withColumn(col, expr(f'cast({col} as double)'))
-
-        predictions = model.transform(data)
-        output = predictions.select(output_ref_cols + [id_col, 'prediction'])
-        print("output is ")
-        print(output)
-
-
 
 
 class TableFuncs:
