c551bbf832d897f5f9ef9f82e5c0c27fd824f85f
yangqinke <qinke.yang@thoughtworks.com>
2022-05-20 17:00:10 +0800

    remove local test

--- test/test_bg_ml.py
+++
@@ -1,6 +0,0 @@
-from easy_sql.sql_processor.funcs_rdb import  ModelFuncs
-ModelFuncs(None)\
-    .bq_model_predict_with_tmp_spark("/Users/qinke.yang/Documents/workbench/dataplat-gcp-demo"
-                                  "/workflow/sales/dm/label/model/demo/v1","ods_sales.sales_model_demo_with_label",
-                                  "ods_sales.sales_model_demo_with_label_out",
-                                  "*","id","label")
